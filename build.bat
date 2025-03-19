067fa195399080ae6f522152d3de70cf1f2e229f
Douglas S. Elias <douglassantoselias@gmail.com>
2025-03-19 11:59:41 -0300

    Add triangle rasterization example file

--- build.bat
+++ build.bat
@@ -12,7 +12,7 @@ rmdir /S /Q .\build
 mkdir build
 pushd .\build
 
-set annoying_warnings=/wd4189 /wd4996 /wd4100 /wd4255 /wd5045 /wd4711 /wd4710 /wd4820 /wd4514 /wd4702 /wd4101
+set annoying_warnings=/wd4189 /wd4996 /wd4100 /wd4255 /wd5045 /wd4711 /wd4710 /wd4820 /wd4514 /wd4702 /wd4101 /wd4201
 set system_libs=user32.lib shell32.lib gdi32.lib winmm.lib opengl32.lib kernel32.lib
 
 cl /nologo /Z7 /diagnostics:caret /sdl /Wall /WX /W4 %annoying_warnings% /MD /I..\vendor ..\main.c /link ..\vendor\raylib.lib %system_libs%
